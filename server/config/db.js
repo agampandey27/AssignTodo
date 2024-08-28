6e7c545126d4d8fff897084f4cb8f190d43242a6
Agam Pandey <agampandey705@gmail.com>
2024-08-28 10:29:03 +0530

    vercel config

--- server/config/db.js
+++ server/config/db.js
@@ -1,22 +1,3 @@
-// import mongoose from 'mongoose';
-// import * as dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const db = process.env.MONGO_URI;
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(db);
-//     console.log('MongoDB Connected...');
-//   } catch (err) {
-//     console.error(err.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
 import mongoose, { mongo } from "mongoose";
 
 const connectDB = (url) => {
