49ccf29f7908914ced8d5f18e4617cb5a75a54f4
Agam Pandey <agampandey705@gmail.com>
2024-08-28 10:16:27 +0530

    Server update

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,32 @@
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+// import * as dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const db = process.env.MONGO_URI;
+// const db = process.env.MONGO_URI;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(db);
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(db);
+//     console.log('MongoDB Connected...');
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = (url) => {
+  mongoose.set("strictQuery", true);
+
+  mongoose.connect(url)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err)=> console.log(err)
+    );
 };
 
-export default connectDB;
+export default connectDB ; 
+
--- server/server.js
+++ server/server.js
@@ -1,23 +1,19 @@
 import express from 'express';
-import connectDB from './config/db.js';
-import authRoutes from './routes/auth.js';
-import todoRoutes from './routes/todos.js';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './config/db.js';
+import authRoutes from './routes/auth.js';
+import todoRoutes from './routes/todos.js';
+
 dotenv.config();
 
-connectDB();
-
 const app = express();
-
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
-
-const PORT = process.env.PORT || 5000;
 
 app.get('/', async (req, res) => {
     res.status(200).json({
@@ -25,4 +21,13 @@ app.get('/', async (req, res) => {
     });
   });
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(5000, () => console.log('Server started on port 5000'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
